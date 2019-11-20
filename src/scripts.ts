6e3ce65e4725c2b408872c577886dc319a3c6dc8
Philipp Legner <philipp@mathigon.org>
2019-11-20 00:34:04 +0000

    Switch to Typescript!!

--- src/scripts.ts
+++ src/scripts.ts
@@ -4,44 +4,47 @@
 // =============================================================================
 
 
+import {$, $N, $body, $html, Browser, replaceSvgImports, ElementView} from '@mathigon/boost';
 
-import { $, $N, $body, $html, Browser } from '@mathigon/boost';
 
 $html.addClass((Browser.isMobile ? 'is' : 'not') + '-mobile');
-Browser.replaceSvgImports();
+replaceSvgImports();
 
-Browser.ready(function() {
+Browser.ready(() => {
   const $content = $('.api-body');
   if (!$content) return;
 
   const $sidebar = $('.sidebar-body');
-  const positions = [];
+  const positions: [number, ElementView, ElementView][] = [];
 
-  let $currentSection;
-  $content.$$('h2, h3, h4').forEach(($el, i) => {
-    const name = $el.$('a').id;
-    let $link, $section;
+  let $currentSection: ElementView;
+  $content.$$('h2, h3').forEach(($el, i) => {
+    const name = $el.$('a')!.id;
+    let $link: ElementView;
+    let $section: ElementView;
 
     if ($el.is('h2')) {
       $link = $N('a', {href: '#' + name, html: $el.text}, $sidebar);
       $currentSection = $section = $N('div', {class: 'hidden'}, $sidebar);
     } else {
-      $link = $N('a', {href: '#' + name, html: name.replace('+', '#').replace('module_', '')}, $currentSection);
+      $link = $N('a', {
+        href: '#' + name, html: name.replace('+', '#').replace('module_', '')
+      }, $currentSection);
       $section = $currentSection;
     }
 
-    Browser.resize(() => positions[i] = [$el.positionTop, $link, $section]);
+    Browser.onResize(() => positions[i] = [$el.positionTop, $link, $section]);
   });
 
 
-  function getActive(s) {
+  function getActive(s: number) {
     for (let p of positions) if (p[0] > s) return p;
   }
 
-  let $activeLink;
-  let $activeSection;
-  $body.on('scroll', function({ top }) {
-    let active = getActive(top);
+  let $activeLink: ElementView;
+  let $activeSection: ElementView;
+  $body.on('scroll', function ({top}) {
+    let active = getActive(top)!;
 
     if (active[1] !== $activeLink) {
       if ($activeLink) $activeLink.removeClass('active');
